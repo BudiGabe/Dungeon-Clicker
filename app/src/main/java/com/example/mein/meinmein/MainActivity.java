2b6af6b7f48e56ed89ee87ba449f4dfc29386aca
Razvan <Razoku15@gmail.com>
2017-04-03 20:41:42 +0300

    Removed Attack, Defend buttons Added Magic button Created context sensitive listener Added gold system

--- app/src/main/java/com/example/mein/meinmein/MainActivity.java
+++ app/src/main/java/com/example/mein/meinmein/MainActivity.java
@@ -18,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
         final ProgressBar Hp_Enemy = (ProgressBar) findViewById(R.id.Hp_Enemy);
         final ProgressBar Hp_Player = (ProgressBar) findViewById(R.id.Hp_Player);
         final TextView Status = (TextView) findViewById(R.id.Status);
-        Button defend = (Button) findViewById(R.id.defend);
-        final Button attack = ((Button) findViewById(R.id.attack));
+        Button Magic = (Button) findViewById(R.id.magic);
+        final TextView GoldAmount = (TextView) findViewById(R.id.goldAmount);
         class Player {
+            private int gold;
+            public void SetGold(){
+                gold=0;
+                GoldAmount.setText("Gold: " + gold);
+            }
+            public void GetGold(){
+                gold += 10 + (int)(Math.random() * 21);
+                GoldAmount.setText("Gold: " + gold);
+            }
             public void Attack() {
                 int hpEnemyCurrent = Hp_Enemy.getProgress();
                 int randomDmgPlayer = 5 + (int)(Math.random() * 16);
@@ -30,39 +39,36 @@ public class MainActivity extends AppCompatActivity {
         class Enemy {
             public void Attack() {
                 int hpPlayerCurrent = Hp_Player.getProgress();
-                int randomDmgEnemy = 5 + (int) (Math.random() * 16);
+                int randomDmgEnemy = 1 + (int) (Math.random() * 6);
                 Hp_Player.setProgress(hpPlayerCurrent - randomDmgEnemy);
             }
         }
         final Player player= new Player();
         final Enemy enemy= new Enemy();
-        Status.setText("Choose an action");
+        Status.setText("Kill as many enemies before you die!");
+        player.SetGold();
         Status.setOnClickListener(new TextView.OnClickListener(){
             public void onClick(View v){
-                Hp_Enemy.setProgress(100);
-                Hp_Player.setProgress(100);
-                Status.setText("Choose an action");
-                attack.setEnabled(true);
-                Status.setEnabled(false);
-            }
-        });
-        Status.setEnabled(false);
-        attack.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                player.Attack();
-                enemy.Attack();
-                if(Hp_Enemy.getProgress() == 0){
-                    Status.setText("You Win!!");
-                    attack.setEnabled(false);
-                    Status.setEnabled(true);
-                }
-                if(Hp_Player.getProgress() == 0){
-                    Status.setText("You lose! :(");
-                    attack.setEnabled(false);
-                    Status.setEnabled(true);
-                }
-            }
-        });
+                if((Hp_Enemy.getProgress() != 0) && (Hp_Player.getProgress() != 0)){
+                    player.Attack();
+                    enemy.Attack();
+                    if(Hp_Player.getProgress() == 0){
+                        Status.setText("YOU DIED");
+                    }
+                } else {
+                    if(Hp_Enemy.getProgress()==0){
+                        Hp_Enemy.setProgress(100);
+                        player.GetGold();
+                    }
+                    if(Hp_Player.getProgress() == 0){
+                        Status.setText("Kill as many enemies before you die!");
+                        Hp_Enemy.setProgress(100);
+                        Hp_Player.setProgress(100);
+                        player.SetGold();
 
+                    }
+                }
+            }
+        });
     }
 }
